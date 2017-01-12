c8846ce4065d20975386038a7af01a94bab711dc
Jelly Beans <Jelly@Beans.com>
2017-01-12 15:53:16 -0600

    Fixed Indentation issues

--- Python/CamelCase.py
+++ Python/CamelCase.py
@@ -1,3 +1,9 @@
+#This program is supposed to convert a string to camelCase
+#split the string into a list
+#capitalize every word EXCEPT for the first item in the list
+#convert back to a string and remove spaces
+#http://stackoverflow.com/questions/1549641/how-to-capitalize-the-first-letter-of-each-word-in-a-string-python
+
 print("Please enter a string: ")
 response = input()
 splitResponse = response.split(' ')
@@ -12,8 +18,3 @@ for words in responseList:
 
 stringAgain = ''.join(responseList)
 print(responseList)
-
-#split the string into a list
-#capitalize every word EXCEPT for the first item in the list
-#convert back to a string and remove spaces
-#http://stackoverflow.com/questions/1549641/how-to-capitalize-the-first-letter-of-each-word-in-a-string-python
--- Python/randomNumber.py
+++ Python/randomNumber.py
@@ -1,14 +1,14 @@
+#The user can guess an inifinite number of times in this program
+number = 6 #sets the variable number
 while True:
+    print("Guess my number")
     userGuess = input()
+    guess = int(userGuess) #converts user string to int
 
-if userGuess == 5:
-    print("You got it right!")
-
-
-#if response < 5:
-#    print("That number is too low")
-#break
-
-#if response > 5:
-#    print("That number is too high!")
-#break
+    if guess > number:
+        print("Your guess is too high")
+    if guess < number:
+        print("Your guess is too low!")
+    if guess == number:
+        print("You got it right!")
+        break
