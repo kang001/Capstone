881a7c6eba5162e799bd5c10901fb51f6f310dca
Jelly Beans <Jelly@Beans.com>
2017-01-12 15:34:33 -0600

    initial commit

---
+++ Python/CamelCase.py
@@ -0,0 +1,19 @@
+print("Please enter a string: ")
+response = input()
+splitResponse = response.split(' ')
+
+responseList = []
+
+for words in responseList:
+
+    singleWord = words[0].upper() + words[1:] #capitalize first character of each word
+    #convertCamel = string.replace(" ","") #removes spaces
+    responseList.append(singleWord)
+
+stringAgain = ''.join(responseList)
+print(responseList)
+
+#split the string into a list
+#capitalize every word EXCEPT for the first item in the list
+#convert back to a string and remove spaces
+#http://stackoverflow.com/questions/1549641/how-to-capitalize-the-first-letter-of-each-word-in-a-string-python
---
+++ Python/randomNumber.py
@@ -0,0 +1,14 @@
+while True:
+    userGuess = input()
+
+if userGuess == 5:
+    print("You got it right!")
+
+
+#if response < 5:
+#    print("That number is too low")
+#break
+
+#if response > 5:
+#    print("That number is too high!")
+#break
