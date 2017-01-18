44b7ae635a572b2811873f7c97f36cb4a8390418
Jelly Beans <Jelly@Beans.com>
2017-01-18 00:17:06 -0600

    Initial Commit

---
+++ Python/Hangman.py
@@ -0,0 +1,10 @@
+#Source: https://inventwithpython.com/chapter9.html
+#This is a game of hangman. The program picks a random word
+
+#here make a list of random words
+
+#here make the program choose a random word
+
+#
+var = hi
+for i in words
